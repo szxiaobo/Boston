c8bca0b9d50bf96c82ef2d24acfff3c979f62d65
Xiaobo <53539226+szxiaobo@users.noreply.github.com>
2022-01-15 10:53:13 -0600

    Need merge the branch of Branch modeling to the Master Brach (#1)
    * Adding the modeling piece
    
    * continue adding the prediction piece
    
    * changing the plot purposely
    
    * Update the scale of x and y axis

--- Documents/2022/MLprojects2022/boston_regression.py
+++ Documents/2022/MLprojects2022/boston_regression.py
@@ -8,3 +8,28 @@ y = df_boston['Value'] # dependent variable
 x = df_boston['Rooms'] # independent variable
 print(y.head())
 print(x.head())
+
+x1=5
+x = sm.add_constant(x1) # adding a constant
+lm = sm.OLS(y,x).fit() # fitting the model
+
+lm.predict(x)
+
+
+#part 3
+lm.summary()
+# Rooms coef: 9.1021
+# Constant coef: - 34.6706
+# Linear equation: 𝑦 = 𝑎𝑥 + 𝑏
+y_pred = 9.1021 * x['Rooms'] - 34.6706
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+# plotting the data points
+sns.scatterplot(x=x['Rooms'], y=y, z=None) # 1 update this line first
+#plotting the line
+sns.lineplot(x=x['Rooms'],y=y_pred, color='red', color2='yellow2') #2 adding the color
+#axes
+plt.xlim(10) # 3a chage it again 
+plt.ylim(110)  # 3b chage it again 
+plt.show()
