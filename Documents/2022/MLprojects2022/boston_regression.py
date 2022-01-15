0918f147f5ad7aea644650fe2caa655ed3d8d09d
szxiaobo <xiaoboguo2005@gmail.com>
2022-01-15 10:32:36 -0600

    continue adding the prediction piece

--- Documents/2022/MLprojects2022/boston_regression.py
+++ Documents/2022/MLprojects2022/boston_regression.py
@@ -14,3 +14,22 @@ x = sm.add_constant(x1) # adding a constant
 lm = sm.OLS(y,x).fit() # fitting the model
 
 lm.predict(x)
+
+
+#part 3
+lm.summary()
+# Rooms coef: 9.1021
+# Constant coef: - 34.6706
+# Linear equation: 𝑦 = 𝑎𝑥 + 𝑏
+y_pred = 9.1021 * x['Rooms'] - 34.6706
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+# plotting the data points
+sns.scatterplot(x=x['Rooms'], y=y)
+#plotting the line
+sns.lineplot(x=x['Rooms'],y=y_pred, color='red')
+#axes
+plt.xlim(0)
+plt.ylim(0)
+plt.show()
